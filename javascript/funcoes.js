d13fb4023bbc12bc9c58192e8bcfc04bbde7203b
Gabrieloczust <38117382+Gabrieloczust@users.noreply.github.com>
2018-06-11 17:19:32 -0300

    11/06

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,4 +1,11 @@
-function mudaFoto(foto)
-{
-  document.getElementById("icone").src = foto;
-}
+var dfAluguel = document.getElementsByName("dfAluguel");
+var dfTransporte = document.getElementsByName("dfTransporte");
+var dfAgua = document.getElementsByName("dfAgua");
+var dfLuz = document.getElementsByName("dfLuz");
+var dfOutros = document.getElementsByName("dfOutros");
+
+var dvFrete = document.getElementsByName("dvFrete");
+var dvComissao = document.getElementsByName("dvComissao");
+var dvIcms = document.getElementsByName("dvIcms");
+var dvCofins = document.getElementsByName("dvCofins");
+var dvOutros = document.getElementsByName("dvOutros");
